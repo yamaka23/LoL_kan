8b8ed1f06648f52f23ddcf23bbb6db0929830f11
SHOGO YAMAKAWA <showsan2727oneok@gmail.com>
2025-07-24 01:19:52 +0900

    Seeder挿入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            LaneSeeder::class,
+            UserSeeder::class,
+            PostSeeder::class,
+            // 他のシーダーもここに追加できます
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+
+        ]);
+        
     }
 }
